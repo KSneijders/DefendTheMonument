2603e235df435044425b3a588b1d3060c3cb45fa
KSneijders <ksneijders-dev@hotmail.com>
2024-04-16 00:28:42 +0200

    Added lose-condition (temp code)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from AoE2ScenarioParser.datasets.buildings import BuildingInfo
+from AoE2ScenarioParser.datasets.players import PlayerId
 from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
 
 from src.builders.movement_builder import MovementBuilder
@@ -23,6 +25,14 @@ spawner_builder.apply(scenario)
 # ...
 movement_builder = MovementBuilder()
 movement_builder.apply(scenario)
+
+# WIN CONDITION
+targets = um.filter_units_by_const(unit_consts=[BuildingInfo.TEMPLE_OF_HEAVEN.ID], player_list=[PlayerId.ONE])
+target = targets[0]
+
+trigger = tm.add_trigger("YOU LOSE HAHA")
+trigger.new_condition.destroy_object(unit_object=target.reference_id)
+trigger.new_effect.declare_victory(source_player=PlayerId.EIGHT)
 
 scenario.write_to_file(f"{folder_de}{filename}_written.aoe2scenario")
 
