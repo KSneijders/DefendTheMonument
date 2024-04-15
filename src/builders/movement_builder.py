673d208ca39f9565d76b02edb677815647d6c62e
KSneijders <ksneijders-dev@hotmail.com>
2024-04-16 00:28:22 +0200

    Added close area task to attack the temple

--- src/builders/movement_builder.py
+++ src/builders/movement_builder.py
@@ -1,5 +1,6 @@
 import math
 
+from AoE2ScenarioParser.datasets.buildings import BuildingInfo
 from AoE2ScenarioParser.datasets.players import PlayerId
 from AoE2ScenarioParser.scenarios.aoe2_scenario import AoE2Scenario
 
@@ -9,7 +10,12 @@ class MovementBuilder:
         pass
 
     def apply(self, scenario: AoE2Scenario) -> None:
-        tm = scenario.trigger_manager
+        um, tm = scenario.unit_manager, scenario.trigger_manager
+
+        CLOSE_TARGET_AREA = 20
+
+        targets = um.filter_units_by_const(unit_consts=[BuildingInfo.TEMPLE_OF_HEAVEN.ID], player_list=[PlayerId.ONE])
+        target = targets[0]
 
         map_size = scenario.map_manager.map_size
         trigger = tm.add_trigger('Movement loop', looping=True)
@@ -20,10 +26,20 @@ class MovementBuilder:
             for y in range(0, map_size, block_size):
                 trigger.new_effect.attack_move(
                     source_player=PlayerId.EIGHT,
-                    location_x=math.floor(map_size / 2),
-                    location_y=math.floor(map_size / 2),
+                    location_x=math.floor(target.x),
+                    location_y=math.floor(target.y),
                     area_x1=x,
                     area_y1=y,
                     area_x2=min(map_size, x + block_size - 1),
                     area_y2=min(map_size, y + block_size - 1),
                 )
+
+        center_area = scenario.new.area().size(CLOSE_TARGET_AREA)
+
+        trigger = tm.add_trigger('Close movement loop', looping=True)
+        trigger.new_condition.timer(30)
+        trigger.new_effect.task_object(
+            source_player=PlayerId.EIGHT,
+            location_object_reference=target.reference_id,
+            **center_area.to_dict()
+        )
