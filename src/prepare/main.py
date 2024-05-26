e55845c67fbfc97a8cdb5285e41bd28890a16847
KSneijders <ksneijders-dev@hotmail.com>
2024-05-26 02:08:46 +0200

    Added improved villager spawning
    spreaded circular instead of hardcoded rectangular

--- src/prepare/main.py
+++ src/prepare/main.py
@@ -10,6 +10,7 @@ from AoE2ScenarioParser.datasets.players import PlayerId
 from AoE2ScenarioParser.datasets.terrains import TerrainId
 from AoE2ScenarioParser.datasets.trigger_lists import DiplomacyState, VisibilityState, Attribute, PanelLocation
 from AoE2ScenarioParser.datasets.units import UnitInfo
+from AoE2ScenarioParser.objects.support.tile import Tile
 from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
 from AoE2ScenarioRms import AoE2ScenarioRms
 from AoE2ScenarioRms.debug import ApplyXsPrint
@@ -46,11 +47,14 @@ scenario = AoE2DEScenario.from_file(f"{folder_de}{filename}.aoe2scenario")
 tm, um, mm, xm, pm, msm = scenario.trigger_manager, scenario.unit_manager, scenario.map_manager, scenario.xs_manager, \
     scenario.player_manager, scenario.message_manager
 
+um.remove_eye_candy()
+
 xm.initialise_xs_trigger()
 
 # ####### UNITS ######## #
 
-CLEAR_AREA = 24
+VILLAGER_SPAWN_DISTANCE = 20
+VILLAGER_SPAWN_CLEAR_DISTANCE = 2
 NO_BUILD_AREA = 6
 
 # Clear all animals, Player units and relics from the map
@@ -59,22 +63,21 @@ ScenarioUtil.clear(scenario, ObjectClear.ANIMAL_OBJECTS | ObjectClear.PLAYERS | 
 # Get area object for the scenario
 area = scenario.new.area().select_entire_map()
 
-center = area.copy().size(1).expand(CLEAR_AREA)
 center_no_build = area.copy().select_entire_map().size(0).expand(NO_BUILD_AREA)
 
-for unit_const in um.get_units_in_area(**center.to_dict(prefix='')):
-    um.remove_unit(unit=unit_const)
+# for unit_const in um.get_units_in_area(**center.to_dict(prefix='')):
+#     um.remove_unit(unit=unit_const)
 
-center_1x1 = center.copy().size(1)
+mm.set_elevation(1, **center_no_build.to_dict(prefix=''))
+
+center_1x1 = area.copy().size(1)
 center_tile = center_1x1.corner1
 center_tile_offset = center_1x1.copy().center(center_tile.x - 1, center_tile.y - 1).corner1
 
 # TODO: !!!!!!! Make NOT DELETABLE !!!!!!!
 um.add_unit(PlayerId.ONE, unit_const=BuildingInfo.TEMPLE_OF_HEAVEN.ID, x=center_tile.x, y=center_tile.y)
 
-villager_spawn_distance = CLEAR_AREA * .8
-
-center_large = center.copy().size(50).use_pattern_grid(block_size=1, gap_size=5)
+center_large = area.copy().size(50).use_pattern_grid(block_size=1, gap_size=5)
 coords = center_large.to_dict()
 
 for tile in center_large.to_coords():
@@ -90,6 +93,7 @@ entire_map_grid = area.copy().use_pattern_grid(block_size=1, gap_size=10)
 for tile in entire_map_grid.to_coords():
     um.add_unit(PlayerId.EIGHT, unit_const=OtherInfo.MAP_REVEALER_GIANT.ID, tile=tile)
 
+# Set allied vision
 for player in players:
     for target in players:
         if player == target:
@@ -97,13 +101,52 @@ for player in players:
         map_revealers_trigger.new_effect.set_player_visibility(player, target, VisibilityState.VISIBLE)
 
 
+def get_furthest_coords(coords, n):
+    if n > len(coords):
+        raise ValueError("n cannot be larger than the number of coordinates")
+
+    selected = [coords[0]]
+    remaining = coords[1:]
+
+    while len(selected) < n:
+        max_min_dist = 0
+        furthest = None
+
+        for point in remaining:
+            min_dist = min(
+                math.hypot(abs(point.x - selected_point.x), abs(point.y - selected_point.y))
+                for selected_point in selected
+            )
+            if min_dist > max_min_dist:
+                max_min_dist = min_dist
+                furthest = point
+        selected.append(furthest)
+        remaining.remove(furthest)
+
+    return selected
+
+
+spawnable = []
+for angle in range(0, 360, 3):
+    radians = math.radians(angle)
+    x = math.floor(center_tile.x + VILLAGER_SPAWN_DISTANCE * math.cos(radians))
+    y = math.floor(center_tile.y + VILLAGER_SPAWN_DISTANCE * math.sin(radians))
+
+    dist = VILLAGER_SPAWN_CLEAR_DISTANCE
+    units = um.get_units_in_area(x - dist, y - dist, x + dist, y + dist, players=[PlayerId.GAIA])
+    units = um.filter_units_by_const([t.ID for t in OtherInfo.trees()], unit_list=units)
+
+    if not units:
+        spawnable.append(Tile(x, y))
+
+player_start_locations = get_furthest_coords(spawnable, len(players))
+
 disable_trigger = tm.add_trigger(f'Disable random player placements')
 disable_trigger.new_condition.timer(3)
-for player in players:
-    um.add_unit(player, unit_const=OtherInfo.MAP_REVEALER_GIANT.ID, tile=center_tile)
+for location_index, start_location in enumerate(player_start_locations):
+    um.add_unit(location_index + 1, unit_const=OtherInfo.MAP_REVEALER_GIANT.ID, tile=center_tile)
 
-    x = center_tile.x + (orientations[player][0] * villager_spawn_distance)
-    y = center_tile.y + (orientations[player][1] * villager_spawn_distance)
+    x, y = start_location
 
     # Nothing works...
     # pm.players[player].initial_camera_x = math.floor(x)
@@ -117,7 +160,8 @@ for player in players:
         xoffset = orientations[i][0] / 2
         yoffset = orientations[i][1] / 2
 
-        villager = um.add_unit(player=player, unit_const=random_vil(), x=x + xoffset, y=y + yoffset)
+        # Default to a player colour if randomized spawns is turned off
+        villager = um.add_unit(player=location_index + 1, unit_const=random_vil(), x=x + xoffset, y=y + yoffset)
         villagers.append(villager)
 
     # Spawn starting resources
@@ -130,8 +174,8 @@ for player in players:
     sheep = []
     for unit_const, repeat in starting_units_per_player:
         while True:
-            sx = (_random.random() * 10 - 5)
-            sy = (_random.random() * 10 - 5)
+            sx = (_random.random() * 8 - 4)
+            sy = (_random.random() * 8 - 4)
 
             if max(abs(sx), abs(sy)) > 2:
                 break
@@ -145,22 +189,29 @@ for player in players:
             fx = x + sx + (xoffset / 1.4)
             fy = y + sy + (yoffset / 1.4)
 
-            unit = um.add_unit(player=PlayerId.GAIA, unit_const=unit_const, x=fx, y=fy, rotation=_random.random() * math.pi * 2)
+            unit = um.add_unit(
+                player=PlayerId.GAIA,
+                unit_const=unit_const,
+                x=fx,
+                y=fy,
+                rotation=_random.random() * math.pi * 2
+            )
             if unit_const == UnitInfo.SHEEP.ID:
                 sheep.append(unit)
 
     # Add triggers & script for random player placement
     for target_player in players:
-        trigger = tm.add_trigger(f'P{target_player} [Section {player}]')
+        trigger = tm.add_trigger(f'P{target_player} [Location {location_index}]')
         func = (
-            f'bool __player_P{target_player}_spawn_section_{player}() {{'
-            f'return (playerShuffleFinished && xsArrayGetInt(playerPositions, {player - 1}) == {target_player - 1});'
+            f'bool __player_P{target_player}_spawn_location_{location_index}() {{'
+            f'return (playerShuffleFinished && xsArrayGetInt(playerPositions, {location_index}) == {target_player - 1});'
             f'}}'
         )
         trigger.new_condition.script_call(func)
         trigger.new_effect.change_ownership(
-            selected_object_ids=[unit.reference_id for unit in villagers + sheep],
+            source_player=location_index + 1,
             target_player=target_player,
+            selected_object_ids=[unit.reference_id for unit in villagers + sheep],
         )
         trigger.new_effect.change_view(
             quantity=0,
@@ -178,9 +229,11 @@ pm.active_players = 8
 tm.add_trigger("-- STARTING RESOURCES --").new_condition.timer(1)
 
 default_res_check_trigger = tm.add_trigger("START RESOURCES == 'LOW'?")
-default_res_check_trigger.new_condition.accumulate_attribute(quantity=1, attribute=Attribute.FOOD_STORAGE, source_player=PlayerId.ONE)
+default_res_check_trigger.new_condition.accumulate_attribute(quantity=1, attribute=Attribute.FOOD_STORAGE,
+                                                             source_player=PlayerId.ONE)
 default_res_check_trigger.new_condition.or_()
-default_res_check_trigger.new_condition.accumulate_attribute(quantity=1, attribute=Attribute.WOOD_STORAGE, source_player=PlayerId.ONE)
+default_res_check_trigger.new_condition.accumulate_attribute(quantity=1, attribute=Attribute.WOOD_STORAGE,
+                                                             source_player=PlayerId.ONE)
 
 # Create trigger to set everyone to default RES
 set_start_res_trigger = tm.add_trigger('SET DEFAULT START RESOURCES')
@@ -234,7 +287,8 @@ for resource in resources:
     name = resource.name.split('_')[0]
 
     map_revealers_trigger = tm.add_trigger(f"GIVE P8 {name}", looping=True)
-    map_revealers_trigger.new_condition.accumulate_attribute(quantity=100_000, attribute=resource, source_player=PlayerId.EIGHT, inverted=True)
+    map_revealers_trigger.new_condition.accumulate_attribute(quantity=100_000, attribute=resource,
+                                                             source_player=PlayerId.EIGHT, inverted=True)
     map_revealers_trigger.new_effect.tribute(-200_000, resource, PlayerId.EIGHT, PlayerId.GAIA)
 
 pm.players[PlayerId.EIGHT].population_cap = 1000
@@ -249,8 +303,6 @@ for terrain in center_no_build.to_coords(as_terrain=True):
     terrain.layer = terrain.layer if terrain.layer != -1 else terrain.terrain_id
     terrain.terrain_id = TerrainId.ROCK_1  # Non-buildable
 
-mm.set_elevation(0, **center.copy().expand(10).to_dict(prefix=''))
-
 # ####### RANDOM RESOURCE SPAWNING ######## #
 
 asr = AoE2ScenarioRms(scenario)
@@ -259,7 +311,8 @@ asr = AoE2ScenarioRms(scenario)
 grid_map = GridMapFactory.block(
     scenario=scenario,
     object_marks=ObjectMark.TREES | ObjectMark.CLIFFS,
-    area=scenario.new.area().select_entire_map().use_only_edge(line_width=math.floor((scenario.map_manager.map_size / 2) - (CLEAR_AREA + 1)))
+    area=scenario.new.area().select_entire_map().use_only_edge(
+        line_width=math.floor((scenario.map_manager.map_size / 2) - (VILLAGER_SPAWN_DISTANCE + 1)))
 )
 asr.create_objects(starting_area_additional_objects_config, grid_map)
 
